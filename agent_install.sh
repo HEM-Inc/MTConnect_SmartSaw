5f70f42a61c0fd98e1c445d1711a4792e0adf5d8
Max Harris <maxharris@hemsaw.com>
2022-07-25 12:15:20 -0500

    updated the pwd folder

--- agent_install.sh
+++ agent_install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+echo "Printing the Working Directory... /n"
+pwd
+
 echo "Installing MTConnect Adapter and setting it as a SystemCTL... /n"
 
 sudo useradd -r -s /bin/false adapter
 
 sudo mkdir -p /etc/adapter/
-sudo cp -r /home/hemsaw/mtconnect/adapter/. /etc/adapter/
-sudo cp -r /home/hemsaw/mtconnect/afg/SmartSaw_DC.afg /etc/adapter/
+sudo cp -r ./adapter/. /etc/adapter/
+sudo cp -r ./afg/SmartSaw_DC.afg /etc/adapter/
 
 sudo chmod +x /etc/adapter/Adapter
 sudo chown -R adapter:adapter /etc/adapter
@@ -33,10 +36,10 @@ sudo mkdir -p /etc/mtconnect/styles/
 sudo cp agent/agent /usr/bin/
 sudo chmod +x /usr/bin/agent
 
-sudo cp -r /home/hemsaw/mtconnect/agent/. /etc/mtconnect/agent/
-sudo cp -r /home/hemsaw/mtconnect/devices/. /etc/mtconnect/devices/
-sudo cp -r /home/hemsaw/mtconnect/schema/. /etc/mtconnect/schema/
-sudo cp -r /home/hemsaw/mtconnect/styles/. /etc/mtconnect/styles/
+sudo cp -r ./agent/. /etc/mtconnect/agent/
+sudo cp -r ./devices/. /etc/mtconnect/devices/
+sudo cp -r ./schema/. /etc/mtconnect/schema/
+sudo cp -r ./styles/. /etc/mtconnect/styles/
 sudo chown -R mtconnect:mtconnect /etc/mtconnect
 
 sudo cp /etc/mtconnect/agent/agent.service /etc/systemd/system/
--- agent_update.sh
+++ agent_update.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "Printing the Working Directory... /n"
+pwd
+
 echo "Updating MTConnect Adapter... /n"
 
 sudo systemctl stop adapter
-sudo cp -r /home/hemsaw/mtconnect/adapter/. /etc/adapter/
-sudo cp -r /home/hemsaw/mtconnect/afg/SmartSaw_DC.afg /etc/adapter/
+sudo cp -r ./adapter/. /etc/adapter/
+sudo cp -r ./afg/SmartSaw_DC.afg /etc/adapter/
 sudo chown -R adapter:adapter /etc/adapter
 sudo chmod +x /etc/adapter/Adapter
 sudo /etc/adapter/adapter.service /etc/systemd/system/
@@ -19,10 +22,10 @@ echo "MTConnect Adapter Up and Running /n"
 echo "Updating MTConnect Agent... /n"
 
 sudo systemctl stop agent
-sudo cp -r /home/hemsaw/mtconnect/agent/. /etc/mtconnect/agent/
-sudo cp -r /home/hemsaw/mtconnect/devices/. /etc/mtconnect/devices/
-sudo cp -r /home/hemsaw/mtconnect/schema/. /etc/mtconnect/schema/
-sudo cp -r /home/hemsaw/mtconnect/styles/. /etc/mtconnect/styles/
+sudo cp -r ./agent/. /etc/mtconnect/agent/
+sudo cp -r ./devices/. /etc/mtconnect/devices/
+sudo cp -r ./schema/. /etc/mtconnect/schema/
+sudo cp -r ./styles/. /etc/mtconnect/styles/
 sudo chown -R mtconnect:mtconnect /etc/mtconnect
 sudo cp agent/agent /usr/bin/
 sudo chmod +x /usr/bin/agent
@@ -33,3 +36,4 @@ sudo systemctl start agent
 sudo systemctl status agent
 
 echo "MTConnect Agent Up and Running /n"
+pwd
