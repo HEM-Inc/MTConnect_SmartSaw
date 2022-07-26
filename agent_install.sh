8fd3f4749bd7085734a80a119daf2e5ee5bacc1d
Max Harris <maxharris@hemsaw.com>
2022-07-26 12:19:56 -0500

    Adding options to the agent files

--- agent_install.sh
+++ agent_install.sh
@@ -1,46 +1,95 @@
 #!/bin/sh
 
-echo "Printing the Working Directory..."
-pwd
+############################################################
+# Help                                                     #
+############################################################
+Help(){
+    # Display Help
+    echo "Add description of the script functions here."
+    echo
+    echo "Syntax: agent_install [-h|-a File_Name|-d File_Name]"
+    echo "options:"
+    echo "-h             Print this Help."
+    echo "-a File_Name   Declare the afg file name; Defaults to - SmartSaw_DC.afg"
+    echo "-d File_Name   Declare the MTConnect agent device file name; Defaults to - SmartSaw_DC.xml"
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run agent_install.sh as sudo" ; exit 1 ; fi
+
+# Set variables
+Afg_File="SmartSaw_DC.afg"
+Device_File="SmartSaw_DC.xml"
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts "h:a:d:" option; do
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        a) # Enter an AFG file name
+            Afg_File=$OPTARG;;
+        d) # Enter a Device file name
+            Device_File=$OPTARG;;
+        \?) # Invalid option
+            echo "Error: Invalid option chosen"
+            Help
+            exit;;
+    esac
+done
+
+echo "Printing the Working Directory and options..."
+echo "Present directory = " pwd
+echo "AFG file = "$Afg_File
+echo "MTConnect Agent file = "$Device_File
 echo ""
 
 echo "Installing MTConnect Adapter and setting it as a SystemCTL..."
 
-sudo mkdir -p /etc/adapter/
-sudo cp -r ./adapter/. /etc/adapter/
-sudo cp -r ./afg/SmartSaw_DC.afg /etc/adapter/
-sudo chmod +x /etc/adapter/Adapter
+mkdir -p /etc/adapter/
+cp -r ./adapter/. /etc/adapter/
+cp -r ./afg/$Afg_File /etc/adapter/
+chmod +x /etc/adapter/Adapter
 
-sudo cp /etc/adapter/adapter.service /etc/systemd/system/
-sudo systemctl enable adapter
-sudo systemctl start adapter
-sudo systemctl status adapter
+cp /etc/adapter/adapter.service /etc/systemd/system/
+systemctl enable adapter
+systemctl start adapter
+systemctl status adapter
 
 echo "MTConnect Adapter Up and Running"
 
 echo "Installing MTConnect and setting it as a SystemCTL..."
 
-sudo useradd -r -s /bin/false mtconnect
-sudo mkdir /var/log/mtconnect
-sudo chown mtconnect:mtconnect /var/log/mtconnect
+useradd -r -s /bin/false mtconnect
+mkdir /var/log/mtconnect
+chown mtconnect:mtconnect /var/log/mtconnect
 
-sudo mkdir -p /etc/mtconnect/
-sudo mkdir -p /etc/mtconnect/agent/
-sudo mkdir -p /etc/mtconnect/devices/
-sudo mkdir -p /etc/mtconnect/schema/
-sudo mkdir -p /etc/mtconnect/styles/
-sudo cp agent/agent /usr/bin/
-sudo chmod +x /usr/bin/agent
+mkdir -p /etc/mtconnect/
+mkdir -p /etc/mtconnect/agent/
+mkdir -p /etc/mtconnect/devices/
+mkdir -p /etc/mtconnect/schema/
+mkdir -p /etc/mtconnect/styles/
+cp agent/agent /usr/bin/
+chmod +x /usr/bin/agent
 
-sudo cp -r ./agent/. /etc/mtconnect/agent/
-sudo cp -r ./devices/. /etc/mtconnect/devices/
-sudo cp -r ./schema/. /etc/mtconnect/schema/
-sudo cp -r ./styles/. /etc/mtconnect/styles/
-sudo chown -R mtconnect:mtconnect /etc/mtconnect
+cp -r ./agent/. /etc/mtconnect/agent/
+sed -i '1 i\Devices = ../devices/'$Device_File /etc/mtconnect/agent/agent.cfg
+cp -r ./devices/$Device_File /etc/mtconnect/devices/
+cp -r ./schema/. /etc/mtconnect/schema/
+cp -r ./styles/. /etc/mtconnect/styles/
+chown -R mtconnect:mtconnect /etc/mtconnect
 
-sudo cp /etc/mtconnect/agent/agent.service /etc/systemd/system/
-sudo systemctl enable agent
-sudo systemctl start agent
-sudo systemctl status agent
+cp /etc/mtconnect/agent/agent.service /etc/systemd/system/
+systemctl enable agent
+systemctl start agent
+systemctl status agent
 
 echo "MTConnect Agent Up and Running"
--- agent_update.sh
+++ agent_update.sh
@@ -1,38 +1,87 @@
 #!/bin/sh
 
-echo "Printing the Working Directory..."
-pwd
+############################################################
+# Help                                                     #
+############################################################
+Help(){
+    # Display Help
+    echo "Add description of the script functions here."
+    echo
+    echo "Syntax: agent_install [-h|-a File_Name|-d File_Name]"
+    echo "options:"
+    echo "-h             Print this Help."
+    echo "-a File_Name   Declare the afg file name; Defaults to - SmartSaw_DC.afg"
+    echo "-d File_Name   Declare the MTConnect agent device file name; Defaults to - SmartSaw_DC.xml"
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run agent_update.sh as sudo" ; exit 1 ; fi
+
+# Set variables
+Afg_File="SmartSaw_DC.afg"
+Device_File="SmartSaw_DC.xml"
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts "h:a:d:" option; do
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        a) # Enter an AFG file name
+            Afg_File=$OPTARG;;
+        d) # Enter a Device file name
+            Device_File=$OPTARG;;
+        \?) # Invalid option
+            echo "Error: Invalid option chosen"
+            Help
+            exit;;
+    esac
+done
+
+echo "Printing the Working Directory and options..."
+echo "Present directory = " pwd
+echo "AFG file = "$Afg_File
+echo "MTConnect Agent file = "$Device_File
 echo ""
 
 echo "Updating MTConnect Adapter..."
 
-sudo systemctl stop adapter
-sudo cp -r ./adapter/. /etc/adapter/
-sudo cp -r ./afg/SmartSaw_DC.afg /etc/adapter/
-sudo chmod +x /etc/adapter/Adapter
-sudo cp /etc/adapter/adapter.service /etc/systemd/system/
+systemctl stop adapter
+cp -r ./adapter/. /etc/adapter/
+cp -r ./afg/$Afg_File /etc/adapter/
+chmod +x /etc/adapter/Adapter
+cp /etc/adapter/adapter.service /etc/systemd/system/
 
-sudo systemctl daemon-reload
-sudo systemctl start adapter
-sudo systemctl status adapter
+systemctl daemon-reload
+systemctl start adapter
+systemctl status adapter
 
 echo "MTConnect Adapter Up and Running"
 echo ""
 
 echo "Updating MTConnect Agent..."
 
-sudo systemctl stop agent
-sudo cp -r ./agent/. /etc/mtconnect/agent/
-sudo cp -r ./devices/. /etc/mtconnect/devices/
-sudo cp -r ./schema/. /etc/mtconnect/schema/
-sudo cp -r ./styles/. /etc/mtconnect/styles/
-sudo chown -R mtconnect:mtconnect /etc/mtconnect
-sudo cp agent/agent /usr/bin/
-sudo chmod +x /usr/bin/agent
-sudo cp /etc/mtconnect/agent/agent.service /etc/systemd/system/
+systemctl stop agent
+cp -r ./agent/. /etc/mtconnect/agent/
+sed -i '1 i\Devices = ../devices/'$Device_File /etc/mtconnect/agent/agent.cfg
+cp -r ./devices/$Device_File /etc/mtconnect/devices/
+cp -r ./schema/. /etc/mtconnect/schema/
+cp -r ./styles/. /etc/mtconnect/styles/
+chown -R mtconnect:mtconnect /etc/mtconnect
+cp agent/agent /usr/bin/
+chmod +x /usr/bin/agent
+cp /etc/mtconnect/agent/agent.service /etc/systemd/system/
 
-sudo systemctl daemon-reload
-sudo systemctl start agent
-sudo systemctl status agent
+systemctl daemon-reload
+systemctl start agent
+systemctl status agent
 
 echo "MTConnect Agent Up and Running"
