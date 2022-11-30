29e4ed181090c209f91be0c9e1603dd4b6a077ff
Max Harris <maxharris@hemsaw.com>
2022-11-30 09:45:09 -0600

    update the device files to remove tension monitor and edit ruby file

--- ruby/module.rb
+++ ruby/module.rb
@@ -1,39 +1,39 @@
+# Sample Override for hours till next service to throw error
+
 class AlertTransform < MTConnect::RubyTransform
-    def initialize(name, filter)
-      @cache = Hash.new
-      super(name, filter)
-    end
-  
-    @@count = 0
-    def transform(obs)
-      @@count += 1
-      if @@count % 10000 == 0
-        puts "---------------------------"
-        puts ">  #{ObjectSpace.count_objects}"
-        puts "---------------------------"
-      end
-      
-      dataItemId = obs.properties[:dataItemId]
-      if dataItemId == 'servotemp1' or dataItemId == 'Xfrt' or dataItemId == 'Xload'
-        @cache[dataItemId] = obs.value
-        device = MTConnect.agent.default_device
-        
-        di = device.data_item('xaxisstate')
-        if @cache['servotemp1'].to_f > 10.0 or @cache['Xfrt'].to_f > 10.0 or @cache['Xload'].to_f > 10
-          newobs = MTConnect::Observation.new(di, "ERROR")
-        else
-          newobs = MTConnect::Observation.new(di, "OK")
-        end
-        forward(newobs)
-      end
-      forward(obs)
-    end
+  def initialize(name, filter)
+    @cache = Hash.new
+    super(name, filter)
   end
+
+  @@count = 0
+  def transform(obs)
+    @@count += 1
+    if @@count % 10000 == 0
+      puts "---------------------------"
+      puts ">  #{ObjectSpace.count_objects}"
+      puts "---------------------------"
+    end
+    
+    dataItemId = obs.properties[:dataItemId]
+    if dataItemId == 'saw_time_till_next_service' or dataItemId == 'saw_operating'
+      @cache[dataItemId] = obs.value
+      device = MTConnect.agent.default_device
       
-  MTConnect.agent.sources.each do |s|
-    pipe = s.pipeline
-    puts "Splicing the pipeline"
-    trans = AlertTransform.new('AlertTransform', :Sample)
-    puts trans
-    pipe.splice_before('DeliverObservation', trans)
+      di = device.data_item('saw_hydraulic_low_level')
+      if @cache['saw_time_till_next_service'].to_f <= 0.0 and @cache['Xfrt'].to_f == "ON"
+        newobs = MTConnect::Observation.new(di, "ERROR")
+      end
+      forward(newobs)
+    end
+    forward(obs)
   end
+end
+      
+MTConnect.agent.sources.each do |s|
+  pipe = s.pipeline
+  puts "Splicing the pipeline"
+  trans = AlertTransform.new('AlertTransform', :Sample)
+  puts trans
+  pipe.splice_before('DeliverObservation', trans)
+end
