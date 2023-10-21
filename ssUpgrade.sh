b01ebc1a8b2ed28dc7dbfbf1e79749f3ed0f16d0
skibum1869 <sk18um@protonmail.com>
2023-10-21 17:17:42 -0500

    fix typo

--- ssUpgrade.sh
+++ ssUpgrade.sh
@@ -78,7 +78,7 @@ Update_Agent(){
         mkdir -p /etc/mtconnect/data/
 
         cp -r ./agent/config/. /etc/mtconnect/config/
-        sed -i '1 i\Devices = /mtconnect/config/devices/'$Device_File /etc/mtconnect/config/agent.cfg
+        sed -i '1 i\Devices = /mtconnect/config/'$Device_File /etc/mtconnect/config/agent.cfg
         cp -r ./agent/config/devices/$Device_File /etc/mtconnect/config/
         sed -i "11 i\        <Device id=\"saw\" uuid=\"HEMSaw_$Serial_Number\" name=\"Saw\">" /etc/mtconnect/config/$Device_File
         cp -r ./agent/data/styles/. /etc/mtconnect/data/styles/
