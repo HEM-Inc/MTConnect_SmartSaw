ee945efb61369bc317c0ba5938064e4d9349b251
Max Harris <maxharris@hemsaw.com>
2023-11-28 12:48:12 -0600

    changes to the status print out

--- ssStatus.sh
+++ ssStatus.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run ssStatus.sh as sudo" ; exit 1 ; fi
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run bash ssStatus.sh as sudo" ; exit 1 ; fi
 
+echo "ODS Status..."
 systemctl status ods
-systemctl status adapter
+echo "<<DONE>>"
+echo
 
+echo "Adapter Status..."
+systemctl status adapter
+echo "<<DONE>>"
+echo
+
+echo "Docker container status for Agent, Mosquitto, and Watchtower..."
 docker-compose ps
+echo "<<DONE>>"
